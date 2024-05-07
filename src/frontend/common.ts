47ba6c7a069008e01f6a48e235a4387ee7103347
luc-blaeser <luc.blaeser@dfinity.org>
2024-05-07 09:30:48 +0200

    Adjust frontend authentication to also work with Azle

--- src/frontend/common.ts
+++ src/frontend/common.ts
@@ -1,3 +1,8 @@
+import { AuthClient } from "@dfinity/auth-client";
+import { backend } from "../declarations/backend";
+import { Actor, ActorSubclass, HttpAgent } from "@dfinity/agent";
+import { _SERVICE } from "../declarations/backend/backend.did";
+
 export function getImageSource(imageData: Uint8Array | number[]) {
     if (imageData != null) {
         const array = Uint8Array.from(imageData);
@@ -6,4 +11,11 @@ export function getImageSource(imageData: Uint8Array | number[]) {
     } else {
         return "";
     }
+}
+
+export async function getBackend(): Promise<ActorSubclass<_SERVICE>> {
+    const authClient = await AuthClient.create();
+    const identity = authClient.getIdentity();
+    (Actor.agentOf(backend) as HttpAgent).replaceIdentity(identity);
+    return backend;
 }
